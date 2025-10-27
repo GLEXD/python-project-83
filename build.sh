61d2dff8d26ab54b29e3007892f7f6f171890ba8
GLEXD <glebushekbur@gmail.com>
2025-10-27 19:47:09 +0400

    Small changes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-export DATABASE_URL=postgresql://analyzer_user:analyzer_pass@localhost:5432/page_analyzer
+. $HOME/.local/bin/env
 
 make install && psql -a -d $DATABASE_URL -f database.sql
